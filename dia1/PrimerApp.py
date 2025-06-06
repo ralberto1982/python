8c7662c9ad405db48aea0a5876a467a6d78b46a2
Alberto <ralberto2312@gmail.com>
2025-06-05 19:27:26 -0500

    finish dia 1

---
+++ dia1/PrimerApp.py
@@ -0,0 +1,14 @@
+print('hola mundo')
+
+
+
+
+# STRINGS o cadenas de texto es todo lo que va entre comillas
+# concatenasmos con + 
+# con barra invertida puedo escribir \ me permite que tome caracteres como parte del contenido
+# con la barra invertida \n me permite salto de linea
+# con barra invertida mas n \t me genera un tabulado o espacio
+# doble me permite que paython lo tome una barra
+print('Edwin' + ' '+ 'como estas' +  "\'me llamo edwin\'" + 'isn\'t' + ' esto es una barra invertida \\')
+# INPUT PARA CAPTURAR DATOS
+print('Tu nombre es: ' + input('Dime tu nombre: ') + ' ' + input('Dime tu apellido'))
---
+++ dia1/reto1.py
@@ -0,0 +1,6 @@
+
+# CREA UN PROGRAMA QUE TE PIDA 2 ARGUMENTOS PARA CREAR UNA PALABRA COMPUESTA PARA EL NOMRE DE UNA CERVEZA
+# LA REPUESTA DEBE IMPRIMIRSE EN 2 LINEAS Y ENTRE COMILLAS
+
+
+print("El nombre para tu cerveza puede ser: \n \' " + input('Dame el nombre de ua mascota o animal favorito: ') + " " + input('cual es tu color favorito: ' ) + " \' ")
